f8e923bc9545901076d864147e911b16024c341e
Matthieu Fauveau <mfauveau@gmail.com>
2015-02-10 17:22:19 -0500

    Implement DirectPost Response

--- src/Message/DirectPostResponse.php
+++ src/Message/DirectPostResponse.php
@@ -1,5 +1,5 @@
 <?php
-namespace Omnipay\AuthorizeNet\Message;
+namespace Omnipay\NMI\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
 use Omnipay\Common\Message\RequestInterface;
@@ -13,51 +13,51 @@ class DirectPostResponse extends AbstractResponse
     public function __construct(RequestInterface $request, $data)
     {
         $this->request = $request;
-        $this->data = $data;
+        parse_str($data, $this->data);
     }
 
     public function isSuccessful()
     {
-
+        return '1' === $this->getCode();
     }
 
     public function getCode()
     {
-
+        return $this->data['response'];
     }
 
     public function getResponseCode()
     {
-
+        return $this->data['response_code'];
     }
 
     public function getMessage()
     {
-
+        return $this->data['responsetext'];
     }
 
     public function getAuthorizationCode()
     {
-
+        return $this->data['authcode'];
     }
 
     public function getAVSResponse()
     {
-
+        return $this->data['avsresponse'];
     }
 
     public function getCVVResponse()
     {
-
+        return $this->data['cvvresponse'];
     }
 
     public function getOrderId()
     {
-
+        return $this->data['orderid'];
     }
 
     public function getTransactionReference()
     {
-
+        return $this->data['transactionid'];
     }
 }
